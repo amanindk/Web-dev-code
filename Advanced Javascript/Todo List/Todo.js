e62582ba53ff2db610ef3227985e93f9a8b537d6
Amandeep <amandeepsingh57537@gmail.com>
2024-03-09 23:10:56 +0530

    todo changes

--- Advanced Javascript/Todo List/Todo.js
+++
@@ -1,7 +0,0 @@
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  let titlec = title.value;
-  let descc = desc.value;
-  localStorage.setItem("todo", JSON.stringify[titlec, descc]);
-  console.log(e);
-});
--- Advanced Javascript/Todo List/Todo_list.js
+++ Advanced Javascript/Todo List/Todo_list.js
@@ -1,24 +1,24 @@
-// const inputbox = document.getElementById("input-box");
-// const listcontainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputbox.value === "") {
+  if (inputBox.value === "") {
     alert("you somthing Wrrite now");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputbox.value;
-    listcontainer.appendChild(li);
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "/u00d7";
     li.appendChild(span);
   }
-  inputbox.value = " ";
+  inputBox.value = " ";
 }
 
-const inputbox = document.querySelector("#input-box");
-const listcontainer = document.querySelector("#list-container");
+// const inputbox = document.querySelector("#input-box");
+// const listcontainer = document.querySelector("#list-container");
 
-inputbox.addEventListener("keyup", function (event) {
+inputBox.addEventListener("keyup", function (event) {
   if (event.key == "Enter") {
     console.log(event.key);
   }
