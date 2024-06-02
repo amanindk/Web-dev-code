44c2decbc141fb9ab30f1774be151d203d245aec
Amandeep <amandeepsingh57537@gmail.com>
2024-06-02 12:27:51 +0530

    Password generate

---
+++ Advanced Javascript/Exercise7_PasswordGenerater.js
@@ -0,0 +1,27 @@
+class Password {
+  constructor() {
+    console.log("welcome to Password Generator");
+    this.pass = "";
+  }
+  generatePassword(len) {
+    let chars = "abcdefghijklmnoqrtuvwxyz";
+    let numbers = "1234567890";
+    let special = "!@#$%^&*()";
+    if (len < 3) {
+      console.log("your password should be atleast 3 charactor long");
+    } else {
+      let i = 0;
+      while (i < len) {
+        this.pass += chars[Math.floor(Math.random() * chars.length)];
+        this.pass += numbers[Math.floor(Math.random() * numbers.length)];
+        this.pass += special[Math.floor(Math.random() * special.length)];
+        i += 3;
+      }
+      this.pass = this.pass.substr(0, len);
+      return this.pass;
+    }
+  }
+}
+
+let p = new Password();
+console.log(p.generatePassword(8));
--- Advanced Javascript/chapter12.js
+++ Advanced Javascript/chapter12.js
@@ -1,6 +1,6 @@
 //Problem 1. write a JS program to print the following after 2 second deloy "Hello " "World"
 
-/*
+
 const a = async (text) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -15,7 +15,7 @@ const a = async (text) => {
   text = await a("world");
   console.log(text);
 })();
-*/
+
 
 //Problem 2. write a js program to find average of number in an arrray using speaad syntax
 
@@ -23,28 +23,28 @@ function sum(a, b, c) {
   return a + b + c;
 }
 let x = [1, 2, 3];
-console.log(sum(...x));
+console.log(sum(...x)); 
 
 //Problem 3. write a js function which resolve a Promise after n seconds. The function takes n as thee parameter.
 // use an IIfe to execute the function with different values of n
 
-const a = async (text, n = 2) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(text);
-    }, 1000 * n);
-  });
-};
+// const a = async (text, n = 2) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(text);
+//     }, 1000 * n);
+//   });
+// };
 
-(async () => {
-  let text = await a("i am resolving afte 1 seconds", 1);
-  console.log(text);
-  text = await a("i am resolving after 4 seconds", 4);
-  console.log(text);
-})();
+// (async () => {
+//   let text = await a("i am resolving afte 1 seconds", 1);
+//   console.log(text);
+//   text = await a("i am resolving after 4 seconds", 4);
+//   console.log(text);
+// })();
 
 //wrritee a simple interst calcultor using JS
-function simpleinterst(p, r, t) {
-  return (p * r * t) / 100;
-}
-console.log(simpleinterst(1000, 8, 2));
+// function simpleinterst(p, r, t) {
+//   return (p * r * t) / 100;
+// }
+// console.log(simpleinterst(1000, 8, 2));
