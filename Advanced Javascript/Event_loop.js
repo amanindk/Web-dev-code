467025a6eee720adbde237d52ccc34bf63ca5934
Amandeep <amandeepsingh57537@gmail.com>
2024-06-28 23:05:00 +0530

    update

---
+++ Advanced Javascript/Event_loop.js
@@ -0,0 +1,13 @@
+// button.addEventListner("click", function onclick() {
+  setTimeout(function timer() {
+    console.log("you clicked the button!");
+  }, 2000);
+// });
+
+console.log("Hi!");
+
+setTimeout(function timeout() {
+  console.log("clicked the button");
+}, 5000);
+
+console.log("Welcome to Aman");
